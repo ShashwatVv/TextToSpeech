ef7b4f051610fffb67426af248cf9f9d10fe84fa
Shashwat Vaibahav <vaibhavshashwat@gmail.com>
2019-09-11 12:01:17 +0530

    Create fileRead.py
    reading a text file and converting to speech

---
+++ fileRead.py
@@ -0,0 +1,17 @@
+//google text to speech API
+//Author : Shashwat Vaibhav
+
+from gtts import gTTS
+import os
+
+MyFile = open('SomeFile.txt', 'r') //create a file with any name(eg. SomeFile.txt)
+                                   //and open it in read mode
+MText = MyFile.read().replace("\n", " ")//replace end of line character
+
+language = "en"
+
+output = gTTS(text=Mtext, lang = language, slow = False)
+
+output.save("Music.mp3")
+
+os.system("start Music.mp3")
