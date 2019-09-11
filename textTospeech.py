0c41f1655aaeec4ff7d2f6ba95d56b671ee054a1
Shashwat Vaibahav <vaibhavshashwat@gmail.com>
2019-09-11 11:55:28 +0530

    Add files via upload
    install gtts package with the command 'pip install gtts' .

---
+++ textTospeech.py
@@ -0,0 +1,14 @@
+//google text to speech API
+//Author : Shashwat Vaibhav
+from gtts import gTTS
+import os
+
+Mtext = "Hello there, Shashwat!"
+
+language = "en"
+
+output = gTTS(text=Mtext, lang = language, slow = False)
+
+output.save("Music.mp3")
+
+os.system("start Music.mp3")
